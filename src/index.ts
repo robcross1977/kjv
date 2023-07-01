3876013457eaad74d125f9b2afcdd91c2214e9b1
Robert Crossland <robertcrossland@crosslandcreative.com>
2023-07-01 12:05:28 -0500

    exporting sorted book names

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export * from "./search/search";
 export * from "./lib/types";
-export { bookNames, chapterCountFrom, verseCountFrom } from "./search/bible-meta";
+export { ValidBookName, bookNames, orderedBookNames, chapterCountFrom, verseCountFrom } from "./search/bible-meta";
